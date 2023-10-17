be733fc07cc94f4ea06b08f7429c6ea76493ac92
Hemant <hemo.hm576@gmail.com>
2023-10-18 01:15:21 +0530

    Day 1 - Create Pages and Project

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from '../src/pages/Home'
+import SignIn from '../src/pages/SignIn'
+import SignOut from '../src/pages/SignOut'
+import Profile from '../src/pages/Profile'
+import About from '../src/pages/About'
 function App() {
   return (
-    <>
-      <div className='bg-slate-700 w-6 h-5'>
-        Hello
-      </div>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignOut />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/about" element={<About />} />
+
+      </Routes>
+    </BrowserRouter>
   )
 }
 
