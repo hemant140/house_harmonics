38850894cf0ff46f1ea0c129dad3f7ceeb15fbb2
Hemant <hemo.hm576@gmail.com>
2023-10-23 00:39:11 +0530

    Connected to MongoDb

--- api/index.js
+++ api/index.js
@@ -1,9 +1,21 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from 'dotenv'
+dotenv.config();
+
+mongoose
+    .connect(process.env.MONGODB)
+    .then(() => {
+        console.log("Connected to MongoDB");
+    })
+    .catch((err) => {
+        console.error('Failed to connect', err);
+    });
 
 const app = express();
 
-const Port = 3000;
+const Port = process.env.PORT;
 
-app.listen(Port, ()=>{
+app.listen(Port, () => {
     console.log(`Server is Running ${Port}!`)
 })
